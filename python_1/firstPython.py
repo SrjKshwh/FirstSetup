ad002893c3e14897af727323d01ec8716eaa60e5
SrjKshwh <kush786srj@gmail.com>
2025-06-12 14:43:02 -0400

    adding first python code

---
+++ python_1/firstPython.py
@@ -0,0 +1,17 @@
+#print("Hello, World!")
+'''print("Hello, World!")
+print("Hello, World!")'''
+x=45
+y=54
+print("Hello, World!")
+print(x+y)
+z=x+y
+
+res = "Result is = "
+def func1():
+    print(res,z)
+
+
+func1()
+
+snackName["1 - chips", "2 - tacos", "3 - flavored soda", "4 - muffin", "5 - cupcake"]
